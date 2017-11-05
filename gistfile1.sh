aa47424c350c9132d078114680c5ec390a61e886
Susma Pant <pant.sushma@gmail.com>
2017-11-05 22:20:03 +0545

    Add files via upload

---
+++ gistfile1.sh
@@ -0,0 +1,14 @@
+
+sudo apt-get update
+sudo apt-get install libxml2
+sudo apt-get install libxslt1.1
+sudo apt-get install libxml2-dev
+sudo apt-get install libxslt1-dev
+sudo apt-get install python-libxml2
+sudo apt-get install python-libxslt1
+sudo apt-get install python-dev
+sudo apt-get install python-setuptools
+
+sudo apt-get install libxml2 libxslt1.1 libxml2-dev libxslt1-dev python-libxml2 python-libxslt1 python-dev python-setuptools 
+
+pip install lxml
